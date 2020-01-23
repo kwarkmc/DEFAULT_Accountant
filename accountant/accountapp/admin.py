b0128d1a55f8cc5984225df27d07a842e80ac501
Kwark Min Chang <kwarkmc@naver.com>
2020-01-23 19:46:29 +0900

    KMC Commit 200123-19:46 | home.html 연결, model 설정

--- accountant/accountapp/admin.py
+++ accountant/accountapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Accountant
 
 # Register your models here.
+
+admin.site.register(Accountant)
--- accountant/accountapp/models.py
+++ accountant/accountapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Accountant(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    text = models.TextField()
+    price = models.IntegerField(default=0, null=True)
+    
+def __str__(self):
+    return self.title
